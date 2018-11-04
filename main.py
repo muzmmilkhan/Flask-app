010ec56876adf78695a59f2157b3d759ece9a56d
muzmmil <muzmmilpathan@gmail.com>
2018-11-04 03:31:18 -0800

    Web App using flask

---
+++ main.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_mail import Mail
+import json
+with open('config.json') as c:
+    params = json.load(c)["params"]
+
+local_server = True
+app = Flask(__name__)
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['gmail-user'],
+    MAIL_PASSWORD=params['gmail-password']
+)
+mail = Mail(app)
+if(local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+db = SQLAlchemy(app)
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False)
+    email = db.Column(db.String(120), unique=True)
+    phone = db.Column(db.String(80), unique=True)       
+    msg = db.Column(db.String(120), unique=True)
+    date = db.Column(db.String(120))
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(21), nullable=False)
+    content = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    img_file = db.Column(db.String(12), nullable=True)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', params=params)
+
+
+@app.route("/post/", methods=['GET'])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', params=params, post=post)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', params=params)
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        dt = datetime.today()
+        entry = Contact(name=name, email=email, phone=phone, msg=message, date=dt)
+        db.session.add(entry)
+        db.session.commit()
+        mail.send_message('New message from ' + name,
+                          sender=email,
+                          recipients=[params['gmail-user']],
+                          body=message + "\n" + phone
+                          )
+    return render_template('Contact.html', params=params)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
